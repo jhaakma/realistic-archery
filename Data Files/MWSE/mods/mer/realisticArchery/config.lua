4bfa39d3b62f47d2442707d5293fce85bd7fe3ac
jhaakma <jchaakma@gmail.com>
2022-06-05 21:05:07 +1200

    Update mod description

--- Data Files/MWSE/mods/mer/realisticArchery/config.lua
+++ Data Files/MWSE/mods/mer/realisticArchery/config.lua
@@ -5,8 +5,11 @@ local this = {}
 --Static Config (stored right here)
 this.static = {
     modName = "Realistic Archery",
-    modDescription =
-    [[Projectiles always do damage when they hit the target, but the accuracy of their trajectory is now based on the shooter's marksman skill.]],
+    modDescription = [[
+Projectiles always do damage when they hit the target, but the accuracy of their trajectory is now based on the shooter's marksman skill.
+Like the vanilla hit chance calculation, the attacker's fatigue, agility and luck also affect projectile accuracy. In addition, projectiles are more accurate when the attacker is sneaking.
+This affects the player as well as NPCs and creatures. For creatures which don't have a Marksman skill, their level is used instead.
+]],
 }
 
 --MCM Config (stored as JSON)
